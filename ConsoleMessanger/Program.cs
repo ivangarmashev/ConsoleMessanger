31bbcce8b04afcb4735e247389767fbbb93acce8
Ivan Garmashev <ivan.garmashev@gmail.com>
2021-06-29 13:30:14 +0300

    first commit

---
+++ ConsoleMessanger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleMessanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
