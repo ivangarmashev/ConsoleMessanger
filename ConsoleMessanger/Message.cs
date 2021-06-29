d9b0dad7b97968a721a9a50df7b8af1cea95cd0b
Ivan Garmashev <ivan.garmashev@gmail.com>
2021-06-29 13:47:14 +0300

    create class Message.cs

---
+++ ConsoleMessanger/Message.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace ConsoleMessanger
+{
+    public class Message
+    {
+        public string UserName { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStamp { get; set; }
+        
+        public Message(string userName, string messageText, DateTime timeStamp)
+        {
+            UserName = userName;
+            MessageText = messageText;
+            TimeStamp = timeStamp;
+        }
+        public Message()
+        {
+            UserName = "Test name";
+            MessageText = "Test text message";
+            TimeStamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            string output = String.Format($"{UserName} <{TimeStamp}>: {MessageText}");
+            return output;
+        }
+    }
+}
